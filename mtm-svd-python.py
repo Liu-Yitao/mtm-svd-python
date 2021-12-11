5b887e4605bcab189621786e59c5b098d6792c11
Liu-Yitao <59553921+Liu-Yitao@users.noreply.github.com>
2021-12-11 21:17:27 +0800

    Update mtm-svd-python.py

--- mtm-svd-python.py
+++ mtm-svd-python.py
@@ -1,35 +1,33 @@
-# Script for MultiTaper Method-Singular Value Decomposition (MTM-SVD) in python
-#
+# Script for MultiTaper Method-Singular Value Decomposition (MTM-SVD) with Monte Carlo test in python
+
 # ------------------------------------------------------------------
-#
-# This script is a direct adaptation of the Matlab toolbox developed by
-# Marco Correa-Ramirez and Samuel Hormazabal at 
-# Pontificia Universidad Catolica de Valparaiso
-# Escuela de Ciencias del Mar, Valparaiso, Chile
-# and is available through 
-# http://www.meteo.psu.edu/holocene/public_html/Mann/tools/tools.php
-#
-# This script was adapted by Mathilde Jutras at McGill University, Canada
-# Copyright (C) 2020, Mathilde Jutras
+
+# This script is a modified version of the Python function developed by
+# Mathilde Jutras at McGill University, Canada[1]. 
+# You can find the original Python code here: 
+# https://github.com/mathildejutras/mtm-svd-python
+
+# This script was adapted by Yitao Liu at Nanjing University of Information Science & Technology, China
+# Copyright (C) 2021, Yitao Liu
 # and is available under the GNU General Public License v3.0
-# 
+
 # The script may be used, copied, or redistributed as long as it is cited as follow:
-# Mathilde Jutras. (2020, July 6). mathildejutras/mtm-svd-python: v1.0.0-alpha (Version v1.0.0). Zenodo. http://doi.org/10.5281/zenodo.3932319
-#
+# []
+
 # This software may be used, copied, or redistributed as long as it is not 
 # sold and that this copyright notice is reproduced on each copy made. 
 # This routine is provided as is without any express or implied warranties.
-#
+
 # Questions or comments to:
-# M. Jutras, mathilde.jutras@mail.mcgill.ca
-#
+# Yitao Liu, liuyitao97@outlook.com
+
 # Last update:
-# July 2020
-#
+# Dec 2021
+
 # ------------------------------------------------------------------
-#
+
 # The script is structured as follows:
-#
+
 # In the main script is found in mtm-svd-python.py
 # In the first section, the user can load the data,
 # assuming the outputs are stored in a netcdf format.
@@ -37,97 +35,123 @@
 # The user will then be asked for which frequencies he wants to plot 
 # the spatial patterns associated with the variability.
 # In the third section, the spatial patterns are plotted and saved
-#
+
 # The required functions are found in mtm_functions.py
-#
+
 # ------------------------------------------------------------------
-#
+
+# Python Package needed:
+# - numpy
+# - scipy
+# - xarray (read the netcdf file)
+# - matplotlib (not necessary, just for plotting)
+
+# You can install the needed Python packages by conda,with the command below
+# ```
+# conda install -c conda-forge numpy scipy xarray matplotlib
+# ```
+
+# [1] Mathilde Jutras. (2020, July 6). mathildejutras/mtm-svd-python: v1.0.0-alpha (Version v1.0.0). Zenodo. http://doi.org/10.5281/zenodo.3932319
+
+# ------------------------------------------------------------------
 
 from mtm_functions import *
 import xarray as xr
-from os import listdir
+# from os import listdir
+from os import system
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import time
 
 # -----------------
 # 1) Load the data
 # -----------------
-
-path = ...
-
-files = listdir(path)
-files.sort()
-
+# the data used in this sample is the SST of CMIP5 CCSM4 piControl
+file_path = 'tos_1x1_CCSM4_piControl.nc'
 # Select the depth, in m
-d = 300 
+d = 0 
+model = 'CCSM4' # name of the model, for naming the output
 
-print('Load data...')
-
-dsl = xr.open_dataset(files[0])
-dt = 1./len(dsl.time) # time step in years
-lon = dsl.NAME_LON_VARIABLE
-lat = dsl.NAME_LAT_VARIABLE
-
-ds = xr.open_mfdataset(files)
-var = ds.NAME_VARIABLE 
-var = var.sel(NAME_DEPTH_VARIABLE=d, method='nearest').values
+var = xr.open_dataset(file_path)['tos'].loc[:,0:65,280:360]
+dt = 1. # time step in years
+lon = var.lon
+lat = var.lat
+# var = var.sel(NAME_DEPTH_VARIABLE=d, method='nearest').values
 
 # Plot map of the variable
 xgrid, ygrid = np.meshgrid(lon,lat)
 plt.pcolor(xgrid, ygrid, var[0,:,:], cmap='jet')
 cbar=plt.colorbar()
+plt.savefig(f'Figs/{model}_data_first_timestep.jpg')
 plt.show()
+plt.clf()
 
 # -------------------
 # 2) Compute the LVF
 # -------------------
 
-print('Apply the MTM-SVD...')
-
 # Slepian tapers
 nw = 2; # bandwidth
 kk = 3; # number of orthogonal windows
 
+# Select the frequence that need & make it a xr.DataArray
+# freq >= 0.01 means period shorter than 100 years
+# freq <= 0.1 means period longer than 10 years
+freq_band = [0.01,0.1] # depends on the study
+
 # Reshape the 2d array to a 1d array
+o2 = var.copy().values.astype(np.float32)
 o2ts = o2.reshape((o2.shape[0],o2.shape[1]*o2.shape[2]), order='F')
 p, n = o2ts.shape
 
 # Compute the LFV
 [freq, lfv] = mtm_svd_lfv(o2ts,nw,kk,dt)
 
-# Compute the confidence intervals
-niter = 1000 # minimum of 1000 iterations
-sl = [.9]
-[conffreq, conflevel] = mtm_svd_conf(o2ts,nw,kk,dt,niter,sl)
+freq_sel_index = np.where((freq>=freq_band[0]) & (freq<=freq_band[1]))
+freq_sel = freq[freq_sel_index]
+lfv_sel = lfv[freq_sel_index]
 
-# Plot the spectrum
+spectrum = xr.DataArray(lfv_sel, coords = [freq_sel], dims=['freq']).rename('lfv')
 
-plt.semilogx(freq,lfv)
-plt.semilogx(conffreq,conflevel[0,:],'--r')
-xt = [1./10., 1./5., 1./2.]
-plt.xlim([1./60., 2.])
-plt.xticks(xt, [1./each for each in xt])
-plt.xlabel('Period [year]') ; plt.ylabel('Variance')
-plt.title('LVF at %i m'%d)
-plt.savefig('Figs/spectrum_%s_%im.jpg'%(model,d))
-plt.clf()
+niter = 1000 # at least 1000
+sl = [0.05,0.1,0.3]
+T1 = time.time()
+[freq, conflevel] = monte_carlo_test(o2ts,niter,sl,len(freq),nw,kk,dt)
+T2 = time.time()
+
+freq_sel_index = np.where((freq>=freq_band[0]) & (freq<=freq_band[1]))
+freq_sel = freq[freq_sel_index]
+conflevel_sel = conflevel[:,freq_sel_index][:,0,:]
+
+print('time cost of confidence: %f min' % ((T2 - T1)/60))
 
 # Display the plot to allow the user to choose the frequencies associated with peaks
-plt.plot(freq, lfv)
-plt.plot(conffreq, conflevel[0,:], '--', c='grey')
+fig = plt.figure(figsize=(6, 4))#,tight_layout=True
+gs = gridspec.GridSpec(1, 1)
+ax = plt.subplot(gs[0, 0])
+ax.semilogx(freq_sel, lfv_sel, '-', c='k')
+ax.semilogx(freq_sel, conflevel_sel[0,:], '--', c='tab:red', label=str(sl[0]), alpha=0.8)
+ax.semilogx(freq_sel, conflevel_sel[1,:], '--', c='tab:orange', label=str(sl[1]), alpha=0.8)
+ax.semilogx(freq_sel, conflevel_sel[2,:], '--', c='tab:blue', label=str(sl[2]), alpha=0.8)
+ax.set_xlim([0.01,.1])
+ax.legend()
+plt.savefig(f'Figs/{model}_spectrum.jpg')
 plt.show()
 plt.clf()
 
-fo = [float(each) for each in input('Enter the frequencies for which there is a significant peak and for which you want to plot the map of variance (separated by commas, no space):').split(',')]
+# save data
+conf_dataarray = xr.DataArray(conflevel_sel, coords=[sl, freq_sel], dims=['siglev','freq']).rename('conflev')
+ds = xr.Dataset()
+ds = ds.merge(spectrum.to_dataset()).merge(conf_dataarray.to_dataset())
+system(f'rm -f {model}_lfv_conf.nc')
+ds.to_netcdf(f'./{model}_lfv_conf.nc')
 
-# --------------------------------
+# -------------------
 # 3) Reconstruct spatial patterns
-# --------------------------------
-
-# Select frequency(ies) (instead of user-interaction selection)
-#fo = [0.02, 0.05, 0.15, 0.19, 0.24, 0.276, 0.38] 
-
+# -------------------
+# Select frequency(ies)
+fo = [0.02, 0.05] 
 # Calculate the reconstruction
 
 vexp, totvarexp, iis = mtm_svd_recon(o2ts,nw,kk,dt,fo)
@@ -135,23 +159,26 @@ vexp, totvarexp, iis = mtm_svd_recon(o2ts,nw,kk,dt,fo)
 # Plot the map for each frequency peak
 
 for i in range(len(fo)):
+    RV = np.reshape(vexp[i],xgrid.shape, order='F')
+    fig, (ax1, ax2) = plt.subplots(2,1,gridspec_kw={'height_ratios':[1,3]},figsize=(5,7))
+    ax1.semilogx(freq_sel, lfv_sel, '-', c='k')
+    ax1.semilogx(freq_sel, conflevel_sel[0,:], '--', c='tab:red', label=str(sl[0]), alpha=0.8)
+    ax1.semilogx(freq_sel, conflevel_sel[1,:], '--', c='tab:orange', label=str(sl[1]), alpha=0.8)
+    ax1.semilogx(freq_sel, conflevel_sel[2,:], '--', c='tab:blue', label=str(sl[2]), alpha=0.8)
+    ax1.plot(freq[iis[i]],lfv[iis[i]],'r*',markersize=10)
+    ax1.set_xlabel('Frequency [1/years]')
+    ax1.set_title('LVF at %i m'%d)
 
-	RV = np.reshape(vexp[i],xgrid.shape, order='F')
+    pc = ax2.pcolor(xgrid, ygrid, RV, cmap='jet', vmin=0, vmax=1.5) 
+    cbar = fig.colorbar(pc, ax=ax2, orientation='horizontal', pad=0.1)
+    cbar.set_label('Variance')
+    ax2.set_title('Variance explained by period %.2f yrs'%(1./fo[i]))
 
-	fig, (ax1, ax2) = plt.subplots(2,1,gridspec_kw={'height_ratios':[1,3]},figsize=(5,7))
+    plt.tight_layout()
+    plt.savefig(f'Figs/{model}_peak_analysis_%s_%im_%.2fyrs.jpg'%(model,d,1./fo[i]))
+    plt.show()
+    plt.clf()
 
-	ax1.plot(freq, lfv)
-	ax1.plot(conffreq, conflevel[0,:], '--', c='grey')
-	ax1.plot(freq[iis[i]],lfv[iis[i]],'r*',markersize=10)
-	ax1.set_xlabel('Frequency [1/years]')
-	ax1.set_title('LVF at %i m'%d)
 
-	pc = ax2.pcolor(xgrid, ygrid, RV, cmap='jet', vmin=0, vmax=50) 
-	cbar = fig.colorbar(pc, ax=ax2, orientation='horizontal', pad=0.1)
-	cbar.set_label('Variance')
-	ax2.set_title('Variance explained by period %.2f yrs'%(1./fo[i]))
+print('finish')
 
-	plt.tight_layout()
-	plt.savefig('Figs/peak_analysis_%s_%im_%.2fyrs.jpg'%(model,d,1./fo[i]))
-	#plt.show()
-	plt.clf()
